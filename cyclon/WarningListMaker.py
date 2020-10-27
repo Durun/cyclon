85e32e2ea974e90966f90eb163e7d114846c8e20
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2020-10-27 18:00:23 +0900

    let WarningListMaker setup bug tables

--- cyclon/WarningListMaker.py
+++ cyclon/WarningListMaker.py
@@ -1,4 +1,5 @@
-from . import runjar
+from . import runjar, runsql
+from .env import setupBugTablesSql
 from subprocess import CompletedProcess
 import logging
 import subprocess
@@ -21,6 +22,11 @@ class WarningListMaker(object):
             ) -> CompletedProcess:
         logging.debug("start WarningList: {}".format(warningDbPath))
 
+        setupResult = self.__setupDatabase(dbPath)
+        if (setupResult != 0):
+            logging.error("failed WarningList: Can't apply SQL > {}".format(dbPath))
+            return setupResult
+
         # NOTE: This program only looks at master branch.
         commitID = self.__getCommitID(gitRepoPath, "master")
 
@@ -38,6 +44,13 @@ class WarningListMaker(object):
         else:
             logging.error("failed WarningList: {}".format(warningDbPath))
 
+        return result
+
+    def __setupDatabase(self, dbPath: str) -> CompletedProcess:
+        result = runsql.run(
+            db=dbPath,
+            sqlFile=setupBugTablesSql
+        )
         return result
 
     def __getCommitID(self, repoPath: str, branch: str) -> str:
