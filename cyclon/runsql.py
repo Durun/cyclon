6f4d8fca9e4d7f60c422abd6254dc347bb4712b5
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2020-10-27 17:46:43 +0900

    create runsql.run()

---
+++ cyclon/runsql.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import subprocess
+import logging
+import time
+from .env import sqlBin
+
+def run(db: Path, sqlFile: Path) -> subprocess.CompletedProcess:
+    elapsedTime = time.perf_counter()
+    command = [sqlBin, db]
+    commandStringList = [str(c) for c in command]
+
+    with open(sqlFile) as file:
+        result = subprocess.run(commandStringList, stdin=file, shell=False)
+
+    elapsedTime = time.perf_counter() - elapsedTime
+    logging.info("finish: " + " ".join(commandStringList) + " < "+str(sqlFile))
+    logging.info("elapsed time: %.1fmin" % (elapsedTime/60))
+    return result
